a9424a0cb022951319850904bb948af0111b45e4
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-03-09 14:57:31 +0700

    bt9

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int max = 0;
+        int max1 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max1 = max;
+                max = arr[i];
+            } else if (max1<arr[i]) {
+                max1 =arr[i];
+            }
+        }
+        System.out.println(max1);
+    }
+}
